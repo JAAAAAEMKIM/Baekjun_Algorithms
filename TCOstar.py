f31f1436e07b80b447ddd7d082c39d0a1a6939b8
JAAAAAEMKIM <43107046+JAAAAAEMKIM@users.noreply.github.com>
2019-02-06 15:17:31 +0900

    Create TCOstar.py

---
+++ TCOstar.py
@@ -0,0 +1,39 @@
+#TCO star
+#Baekjun 2448 별 찍기-11
+# with Tail recursion by python3
+
+from math import log2
+import time
+
+def TCOstar(n, before):
+    m = n + 2
+    while True:
+        if n == 1:
+            return before  # before is a list
+        else:
+            k = m - n
+            blank_num = 3 * 2 ** (k - 2)
+            blank_height = 3 * 2 ** (k - 2)
+            result = []
+            for i in range(0, blank_height):
+                result.append([blank_num * " " + before[i][0] + blank_num * " "])
+            for i in range(0, blank_height):
+                result.append([2 * before[i][0]])
+            before = result
+            n -= 1
+
+
+N = int(input(' '))
+
+start = time.time()
+
+k = int(log2(N / 3)) + 1
+firstar = [['  *   '],
+           [' * *  '],
+           ['***** ']]
+
+for items in TCOstar(k, firstar):
+    print(items[0])
+
+end = time.time()
+print("%d seconds taken" %(start - end))
