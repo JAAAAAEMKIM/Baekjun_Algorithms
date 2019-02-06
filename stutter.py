743cd2ce24d27d01aa720d980ee5c57153cd2f13
JAAAAAEMKIM <43107046+JAAAAAEMKIM@users.noreply.github.com>
2019-02-07 00:49:11 +0900

    Add files via upload

---
+++ stutter.py
@@ -0,0 +1,12 @@
+"""
+Baekjun 2675 repeating string
+retrieved from https://www.acmicpc.net/problem/2675
+"""
+T = int(input())
+while T:
+    R, S = input().split(' ')
+    for i in S:
+        print(i*int(R), end='')
+    print('')
+    T -= 1
+
