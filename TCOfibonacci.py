7a03c1e1ab43f9540182fe65f330603379004668
JAAAAAEMKIM <43107046+JAAAAAEMKIM@users.noreply.github.com>
2019-02-17 17:10:54 +0900

    Create TCOfibonacci.py

---
+++ TCOfibonacci.py
@@ -0,0 +1,21 @@
+# Baekjun 2747 fibonacci
+# retrieved from https://www.acmicpc.net/problem/2747
+# in python 3.6
+
+
+def TCOfibonacci(n, before, bbefore):
+    while True:
+        if n == 0:
+            return 0
+
+        elif n == 1:
+            return bbefore
+
+        elif n == 2:
+            return before
+
+        else:
+            temp = bbefore
+            bbefore = before
+            before += temp
+            n -= 1
