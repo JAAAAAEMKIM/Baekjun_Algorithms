e5de46ab3338949b981edf5b775e08c22ab6af91
JAAAAAEMKIM <43107046+JAAAAAEMKIM@users.noreply.github.com>
2019-02-18 20:05:45 +0900

    Update Baekjun_2675_stutter.py

--- Baekjun_2675_stutter.py
+++ Baekjun_2675_stutter.py
@@ -1,7 +1,8 @@
-"""
-Baekjun 2675 repeating string
-retrieved from https://www.acmicpc.net/problem/2675
-"""
+# Baekjun 2675 repeating string
+# retrieved from https://www.acmicpc.net/problem/2675
+# in python 3.6
+
+
 T = int(input())
 while T:
     R, S = input().split(' ')
@@ -9,4 +10,3 @@ while T:
         print(i*int(R), end='')
     print('')
     T -= 1
-
