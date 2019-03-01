e86db961470b1f095320f42532c0291764603d9f
JAAAAAEMKIM <43107046+JAAAAAEMKIM@users.noreply.github.com>
2019-03-01 18:43:42 +0900

    Update Baekjun_2504_Paren_value.cpp

--- Baekjun_2504_Paren_value.cpp
+++ Baekjun_2504_Paren_value.cpp
@@ -10,7 +10,9 @@
 
 using namespace std;
 
-// def paren check function
+
+// def paren-completeness-check function
+
 int check_parenthesis(string s)
 {
     stack<char> stack__;
@@ -45,96 +47,93 @@ int check_parenthesis(string s)
         return 0;
 }
 
+
 // def value calculating function by recursion
-int value_(string s, int rec)
+
+int value_(string s, int rec)   // rec: checking recursion depth
 {
+    // Initialize
     stack<char> stack_;     // I used stack.
     int val_list[15];       // holds 2s and 3s.
     int ndx_list[15];       // holds the index.
     int i=0, j=0;           // to iteratively point the index.
     int result=0;
-    if(s == "()")
-        return 2;
-    else if(s == "[]")
-        return 3;
-    else
+ 
+    // Operation 
+    for(auto c: s)      // for loop
     {
-        for(auto c: s)      // for loop
+        if(stack_.empty())  // if stack is empty
         {
-            if(stack_.empty())  // if stack is empty
-            {
-                if(c =='(')                 // storing the index of the 
-                {                           // beginning points of substrings, 
-                    stack_.push(c);         // and the multiplying values in order.
-                    ndx_list[j] = i;    
-                    val_list[j++] = 2;    // if '(' multiplying value will be 2
-                }
-                else if(c =='[')
-                {
-                    stack_.push(c);
-                    ndx_list[j] = i;
-                    val_list[j++] = 3;    // if '[' multiplying value will be 3
-                }
+            if(c =='(')                 // storing the index of the 
+            {                           // beginning points of substrings, 
+                stack_.push(c);         // and the multiplying values in order.
+                ndx_list[j] = i;    
+                val_list[j++] = 2;    // if '(' multiplying value will be 2
             }
-            
-            else        //if stack is not empty
+            else if(c =='[')
             {
-                if(c =='(')
-                {
-                    stack_.push(c);
-                }
-                else if(c =='[')
-                {
-                    stack_.push(c);
-                }
-                else if(c ==')')
-                {
-                    if (stack_.top()=='(')
-                    {
-                        stack_.pop();
-                    }
-                    else
-                        break;
-                }
-                else    // c == ']'
-                {
-                    if (stack_.top()=='[')
-                    {
-                        stack_.pop();
-                    }
-                    else
-                        break;
-                }
+                stack_.push(c);
+                ndx_list[j] = i;
+                val_list[j++] = 3;    // if '[' multiplying value will be 3
             }
-            i++;
-        }
-        ndx_list[j] =i;
-        for(int k = 0; k<j; k++)
-        {
-            int pre = ndx_list[k];
-            int idx = ndx_list[k+1];
-            int val = val_list[k];
-            string sub;
-            sub = s.substr(pre+1, idx-pre-2);
-            if (sub.length() ==0 && s[pre] =='(')
-            {
-                result += val;
-            }
-            else if (sub.length() ==0 && s[pre] =='[')
-            {
-                result += val;
-            }
-            else
-            {
-                //cout <<"sub is: "<<sub<<'\n';
-                result += val*value_(sub, ++rec);
-            }
-            //cout<<"from "<<rec<<"th recursion, returning: "<<result<<'\n';
         }
 
-        return result;
+        else        //if stack is not empty
+        {
+            if(c =='(')
+            {
+                stack_.push(c);
+            }
+            else if(c =='[')
+            {
+                stack_.push(c);
+            }
+            else if(c ==')')           // closing parens ')'
+            {
+                if (stack_.top()=='(') // check if it matches
+                {
+                    stack_.pop();
+                }
+                else
+                    break;
+            }
+            else    // c == ']'        // closing parens ']'
+            {
+                if (stack_.top()=='[') // check if it matches
+                {
+                    stack_.pop();
+                }
+                else
+                    break;
+            }
+        }
+        i++;    // iterator ++
+    }  // End of for loop
+ 
+    // Starting calculation
+    ndx_list[j] =i;             // Adding the last index
+    for(int k = 0; k<j; k++)    // for loop for j times; j is the number of substructures.
+    {                           // (We increased 'j' when we found the cleavage.)
+        int pre = ndx_list[k];
+        int idx = ndx_list[k+1];
+        int val = val_list[k];
+        string sub;
+        sub = s.substr(pre+1, idx-pre-2);
+        
+        // Conditioning
+        if (sub.length() ==0)  // If minimum structure (cf. () or [])
+        {
+            result += val;     
+        }
+        else                   // Else recursively calculate the value.
+        {
+            result += val*value_(sub, ++rec);  
+        }
     }
-}
+        return result;
+} 
+// End of function def.
+
 
 int main()
 {
@@ -148,11 +147,11 @@ int main()
     int R=0;
 
     //operation
-    if(check_parenthesis(samp))
+    if(check_parenthesis(samp))    // If parenthesis is complete
     {
-        R = value_(samp,1);
+        R = value_(samp,0);        // Use function 'value_'.
         cout << R<<'\n';
     }
-    else{cout <<0;}
+    else{cout <<0;}                // If parenthesis is NOT complete
     return 0;
-}
+} // End of solution
